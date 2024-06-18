16dba35f79545891c63897c48b34ee4c2bb5190f
mrkhairullah <mrkhairullah.id@gmail.com>
2024-06-19 04:22:45 +0700

    Use API data statistic

--- src/scripts/view/pages/dashboard/partner/partnerDashboard.js
+++ src/scripts/view/pages/dashboard/partner/partnerDashboard.js
@@ -1,4 +1,5 @@
 import { setLayoutDashboard } from '../../../templates/template-creator';
+import Cookies from '../../../../utils/cookies.';
 
 const partnerDashboard = {
   async render() {
@@ -28,81 +29,74 @@ const partnerDashboard = {
 
   async afterRender() {
     setLayoutDashboard(2);
-    const lastTransactionComponent = document.querySelector('last-transaction');
-    const data = {
-      company_image: 'https://yt3.googleusercontent.com/ytc/AIdro_ndItzJEgeNIMkMeMRnFGFjUCxCmCvmzQZ_s-MYCEp6fA=s900-c-k-c0x00ffffff-no-rj',
-      company_name: 'PT Asri Indah',
-      price: 150000,
-      status: 'Diproses',
-      date: '23 April 2024',
-      weight: 30,
-    };
 
-    for (let i = 0; i < 3; i += 1) {
-      const transactionItem = document.createElement('div');
-      transactionItem.classList.add('grid', 'grid-cols-5', 'gap-2', 'content-center', 'justify-between', 'w-full', 'h-full');
-      transactionItem.innerHTML = `
-        <div class="flex flex-col gap-4 w-full h-full col-span-2 md:flex-row">
-          <img src="${data.company_image}" alt="Foto Profil Mitra" class="w-[50px] h-[50px] rounded-2xl">
-          <div class="flex flex-col place-content-center">
-            <p class="text-sm font-medium break-words">${data.company_name}</p>
-            <p class="text-sm font-regular text-gray-400 break-words">${data.date}</p>
+    try {
+      const {
+        lastTransactions, totalWeightThisMonth, totalIncomeThisMonth, salesRecapData,
+      } = await fetch(`https://backend-recyclelify.vercel.app/api/statistic/purchase/${Cookies.getUserId()}`);
+
+      const lastTransactionComponent = document.querySelector('last-transaction');
+
+      lastTransactions.forEach((transaction) => {
+        const transactionItem = document.createElement('div');
+        transactionItem.classList.add('grid', 'grid-cols-5', 'gap-2', 'content-center', 'justify-between', 'w-full', 'h-full');
+        transactionItem.innerHTML = `
+          <div class="flex flex-col gap-4 w-full h-full col-span-2 md:flex-row">
+            <img src="${transaction.partner.photo}" alt="Foto Profil Mitra" class="w-[50px] h-[50px] rounded-2xl">
+            <div class="flex flex-col place-content-center">
+              <p class="text-sm font-medium break-words">${transaction.partner.name}</p>
+              <p class="text-sm font-regular text-gray-400 break-words">${transaction.updatedAt}</p>
+            </div>
           </div>
-        </div>
-        <div class="grid grid-cols-3 gap-2 col-span-3 w-full items-center">
-          <p class="text-sm font-regular break-words">${data.weight} kg</p>
-          <p class="text-sm font-regular break-words">Rp. ${data.price}</p>
-          <p class="text-sm font-regular break-words">${data.status}</p>
-        </div>
-      `;
-      lastTransactionComponent.querySelector('#transaction-list').append(transactionItem);
+          <div class="grid grid-cols-3 gap-2 col-span-3 w-full items-center">
+            <p class="text-sm font-regular break-words">${transaction.weight} kg</p>
+            <p class="text-sm font-regular break-words">Rp. ${transaction.weight * transaction.pricePerKilogram}</p>
+            <p class="text-sm font-regular break-words">${transaction.status}</p>
+          </div>
+        `;
+        lastTransactionComponent.querySelector('#transaction-list').append(transactionItem);
+      });
+
+      // Bar chart
+      const barChartComponent = document.querySelector('bar-chart');
+      const barChartData = {
+        labels: ['Minggu-1', 'Minggu-2', 'Minggu-3', 'Minggu-4', 'Minggu-5'],
+        datasets: [
+          {
+            label: 'Total Sampah',
+            data: totalWeightThisMonth,
+            backgroundColor: '#65A30D',
+            borderColor: '#65A30D',
+            borderRadius: 8,
+          },
+        ],
+      };
+      barChartComponent.chartData = barChartData;
+
+      // Line chart
+      const lineChartComponent = document.querySelector('line-chart');
+      const lineChartData = {
+        labels: ['Minggu-1', 'Minggu-2', 'Minggu-3', 'Minggu-4', 'Minggu-5'],
+        datasets: [
+          {
+            label: 'Pendapatan',
+            data: totalIncomeThisMonth,
+            backgroundColor: '#65A30D',
+            borderColor: '#65A30D',
+            borderWidth: 1,
+            borderRadius: 20,
+          },
+        ],
+      };
+      lineChartComponent.chartData = lineChartData;
+
+      // Input data into sales-recap
+      const salesRecapComponent = document.querySelector('sales-recap');
+
+      salesRecapComponent.salesRecapData = salesRecapData;
+    } catch (error) {
+      console.error('PARTNER DASHBOARD STATISTIC:', error);
     }
-    // Bar chart
-    const barChartComponent = document.querySelector('bar-chart');
-    const barChartData = {
-      labels: ['Minggu-1', 'Minggu-2', 'Minggu-3', 'Minggu-4', 'Minggu-5'],
-      datasets: [
-        {
-          label: 'Total Sampah',
-          data: [30, 20, 30, 22, 10],
-          backgroundColor: '#65A30D',
-          borderColor: '#65A30D',
-          borderRadius: 8,
-        },
-      ],
-    };
-    barChartComponent.chartData = barChartData;
-
-    // Line chart
-    const lineChartComponent = document.querySelector('line-chart');
-    const lineChartData = {
-      labels: ['Minggu-1', 'Minggu-2', 'Minggu-3', 'Minggu-4', 'Minggu-5'],
-      datasets: [
-        {
-          label: 'Pendapatan',
-          data: [50000, 20000, 10000, 35000, 5000],
-          backgroundColor: '#65A30D',
-          borderColor: '#65A30D',
-          borderWidth: 1,
-          borderRadius: 20,
-        },
-      ],
-    };
-    lineChartComponent.chartData = lineChartData;
-
-    // Calculate totalWeight, totalIncome from chart
-    const totalWeight = barChartData.datasets[0].data.reduce((acc, value) => acc + value, 0);
-    const totalIncome = lineChartData.datasets[0].data.reduce((acc, value) => acc + value, 0);
-
-    // Input data into sales-recap
-    const salesRecapComponent = document.querySelector('sales-recap');
-    const salesRecapData = {
-      total_weight: totalWeight,
-      total_partner: 6,
-      total_income: totalIncome,
-      total_transaction: 50,
-    };
-    salesRecapComponent.salesRecapData = salesRecapData;
   },
 };
 
